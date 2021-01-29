635ae25af489cc7703f40dc131300d6399d28517
gschievelbein <gschievelbein@kaist.ac.kr>
2021-01-29 20:13:33 +0900

    keeps trying forever to download all missing images

--- app/instagram_image_scraper.py
+++ app/instagram_image_scraper.py
@@ -24,20 +24,16 @@ def remove_existing_images(i_img_dict, out_dir):
     files = os.listdir(out_dir)
     files = [os.path.splitext(file)[0] for file in files]
     for key in files:
-        i_img_dict.pop(key)
+        i_img_dict.pop(key, None)
     return i_img_dict
 
 
 def tor_img_download_loop(i_img_dict, out_dir, headers):
-    i_img_dict = remove_existing_images(i_img_dict, out_dir)
-    if len(i_img_dict) > 0:
-        with TorRequests() as tor_requests:
-            with tor_requests.get_session() as sess:
-                print("Image download tor circuit built.")
-                for k, v in tqdm((i_img_dict.items())):
-                    download_img(sess, k, v, out_dir, headers)
-    else:
-        print('No new images do download')
+    with TorRequests() as tor_requests:
+        with tor_requests.get_session() as sess:
+            print("Image download tor circuit built.")
+            for k, v in tqdm((i_img_dict.items())):
+                download_img(sess, k, v, out_dir, headers)
 
 
 def download_img(session, i_img_name, i_img_link, out_dir, headers):
@@ -53,11 +49,15 @@ def download_img(session, i_img_name, i_img_link, out_dir, headers):
 
 
 def download_images(d_img_dict, out_dir, headers, media_wait_between_requests):
-    try:
-        func_timeout(media_wait_between_requests, tor_img_download_loop, args=[d_img_dict, out_dir, headers])
-    except FunctionTimedOut:
-        print("Torsession terminated after {} seconds tor_timeout.".format(media_wait_between_requests))
-        return
+    while True:
+        d_img_dict = remove_existing_images(d_img_dict, out_dir)
+        if len(d_img_dict) > 0:
+            try:
+                func_timeout(media_wait_between_requests, tor_img_download_loop, args=[d_img_dict, out_dir, headers])
+            except FunctionTimedOut:
+                print("Torsession terminated after {} seconds tor_timeout.".format(media_wait_between_requests))
+        else:
+            break
 
 
 def main():
